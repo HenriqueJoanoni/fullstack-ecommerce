28c67abbcb06c3eec67a8c4a38d70d4e5b28c4d6
chealy90 <christopherhealy20@gmail.com>
2025-01-14 02:18:49 +0000

    browser routing set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
 import Home from "./components/Home";
 import ProductsPage from "./components/ProductsPage";
 
@@ -6,8 +7,14 @@ import ProductsPage from "./components/ProductsPage";
 export default class App extends Component {
     render() {
         return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/products" component={ProductsPage} />
+                </Switch>
+            </BrowserRouter>
             /* <Home/> */
-            <ProductsPage/> 
+            
 
         )
     }
