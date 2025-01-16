48af71d148a80d3bc558c7d1889982a2136e6566
HenriqueJoanoni <henrique.joanoni@gmail.com>
2025-01-16 01:19:03 +0000

    build(client-side): Creating Login/auth feature

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+
+
+export default class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="login-form">Login Page</div>
+        )
+    }
+}
---
+++ client/src/images/index.js
@@ -0,0 +1,16 @@
+/** ALL IMAGE IMPORTS HERE  */
+import guitarFootage from './guitarfootage.mp4';
+import heartIcon from './hearticon.png';
+import shoppingBagIcon from './shoppingbag.png';
+import downArrowIcon from './down-arrow.png';
+import guitarSample from './guitarsample.jpg';
+import userIcon from './user.png';
+
+export {
+    guitarFootage,
+    heartIcon,
+    shoppingBagIcon,
+    downArrowIcon,
+    guitarSample,
+    userIcon,
+};
