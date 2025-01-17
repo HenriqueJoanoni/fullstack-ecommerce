fb123ffa3c007ec90128394597d91ed0c9aa7787
chealy90 <christopherhealy20@gmail.com>
2025-01-17 16:06:58 +0000

    code fixes

--- server/routes/products.js
+++ server/routes/products.js
@@ -26,19 +26,19 @@ router.get(`/products/:_id`, (req, res) => {
             res.json(data)
         }
     })
-}
-
-
-  router.post("/new-product", async (req, res) => {
-    try {
-        const product = req.body
-        const newProduct = new Product(product)
-        const savedProduct = await newProduct.save()
-        res.status(201).json(savedProduct)
-    } catch (error) {
-        console.log(error)
-        res.status(400).send({error: error.message})
-    }
 })
+
+
+router.post("/new-product", async (req, res) => {
+try {
+    const product = req.body
+    const newProduct = new Product(product)
+    const savedProduct = await newProduct.save()
+    res.status(201).json(savedProduct)
+} catch (error) {
+    console.log(error)
+    res.status(400).send({error: error.message})
+}})
+
 
 module.exports = router
