8101bff4705a3f077c158e729c836aee0f6ebd1b
Vialsiu <D00265675@student.dkit.ie>
2025-02-01 20:44:41 +0000

    Products display page is fixed, but open card view still needs to be fixed

--- client/src/components/ProductsPage.js
+++ client/src/components/ProductsPage.js
@@ -1,162 +1,143 @@
-import React, {Component} from 'react';
-import axios from "axios"
+import React, { Component } from 'react';
+import axios from "axios";
 import Header from "./Header";
 import SearchTools from './SearchTools';
 import ProductDisplayCard from './ProductDisplayCard';
 import PageFooter from "./PageFooter";
-import {SERVER_HOST} from "../config/global_constants"
-
+import { SERVER_HOST } from "../config/global_constants";
 
 export default class ProductsPage extends Component {
-    constructor(props) {
-        super(props);
-        /*
-        this.products = [
-            {
-                id: 1,
-                name: "first acoustic",
-                description: "this is a description",
-                price: 2500,
-                mainImage: "./guitarSample.jpg",
-                photos: ["./guitarSample.jpg"],
-                tags: ["acoustic"]
-            },
-            {
-                id: 2,
-                name: "second electric",
-                description: "and this is another description",
-                price: 1700,
-                mainImage: "./guitarSample.jpg",
-                photos: ["./guitarSample.jpg"],
-                tags: ["electric"]
-            },
-            {
-                id: 3,
-                name: "third bass",
-                description: "third this is a description",
-                price: 1200,
-                mainImage: "./guitarSample.jpg",
-                photos: ["./guitarSample.jpg"],
-                tags: ["bass"]
-            },
-            {
-                id: 4,
-                name: "fourth electroacoustic guitar1",
-                description: "fourth this is yet another description",
-                price: 2100,
-                mainImage: "./guitarSample.jpg",
-                photos: ["./guitarSample.jpg"],
-                tags: ["electroacoustic"]
-            }
-        ]
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      selectedTags: [],
+      searchQuery: "",
+      sortField: "name",
+      sortDirection: 1
+    };
+  }
 
-    */
+  determineSelectedProducts = () => {
+    let updatedSelectedProducts = [...this.state.products];
 
+    // Filter from search
+    if (this.state.searchQuery !== "") {
+      updatedSelectedProducts = updatedSelectedProducts.filter(
+        product => product.product_name.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+      );
+    }
 
-        this.state = {
-            products: [],
-            selectedTags: [],
-            searchQuery: "",
-            sortField: "name",
-            sortDirection: 1
+    // Filter from tags
+    if (this.state.selectedTags.length !== 0) {
+      updatedSelectedProducts = updatedSelectedProducts.filter(
+        product => product.tags.some(tag => this.state.selectedTags.includes(tag))
+      );
+    }
+
+    return updatedSelectedProducts;
+  };
+
+  updateSearchQuery = e => {
+    this.setState({ searchQuery: e.target.value });
+    this.determineSelectedProducts();
+  };
+
+  updateSort = val => {
+    switch (val) {
+      case "name_a_z":
+        this.setState({ sortField: "product_name", sortDirection: 1 });
+        break;
+      case "name_z_a":
+        this.setState({ sortField: "product_name", sortDirection: -1 });
+        break;
+      case "price_l_h":
+        this.setState({ sortField: "product_price", sortDirection: 1 });
+        break;
+      case "price_h_l":
+        this.setState({ sortField: "product_price", sortDirection: -1 });
+        break;
+      default:
+        break;
+    }
+  };
+
+  toggleTag = tagName => {
+    if (!this.state.selectedTags.includes(tagName)) {
+      this.setState({ selectedTags: [...this.state.selectedTags, tagName] });
+    } else {
+      let i = this.state.selectedTags.indexOf(tagName);
+      let newTagsList = [...this.state.selectedTags];
+      newTagsList.splice(i, 1);
+      this.setState({ selectedTags: newTagsList });
+    }
+  };
+
+  sortProducts = productsList => {
+    return productsList.sort((a, b) =>
+      a[this.state.sortField] > b[this.state.sortField] ? this.state.sortDirection : -this.state.sortDirection
+    );
+  };
+
+  componentDidMount() {
+    axios.get(`${SERVER_HOST}/products`)
+      .then(res => {
+        if (res.data) {
+          if (res.data.errorMessage) {
+            console.log(res.data.errorMessage);
+          } else {
+            this.setState({ products: res.data });
+          }
         }
-    }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-    determineSelectedProducts = () => {
-        // console.log(this.state.selectedTags)
-        let updatedSelectedProducts = [...this.state.products]
-        //filter from search
-        if (this.state.searchQuery !== "") {
-            updatedSelectedProducts = updatedSelectedProducts.filter(
-                product => product.product_name.toLowerCase().includes(this.state.searchQuery.toLowerCase()
-                )
-            )
-        }
+render() {
+    return (
+      <div id="productsPage">
+        <Header />
+        <div className="products-container">
+          {/* ------filters column*/}
+          <div className="filters-column">
+            <div className="filters-section">
+              <h3>Filter Results</h3>
+              <SearchTools
+                searchQuery={this.state.searchQuery}
+                updateSearchQuery={this.updateSearchQuery}
+                toggleTag={this.toggleTag}
+                updateSort={this.updateSort}
+              />
+            </div>
+          </div>
 
-        //filter from tags
-        if (this.state.selectedTags.length !== 0) {
-            updatedSelectedProducts = updatedSelectedProducts.filter(
-                product => product.tags.some(tag => this.state.selectedTags.includes(tag)
-                )
-            )
-        }
+          <div className="products-display">
+            {/* ---------sort header*/}
+            <div className="sort-header">
+              <span>Showing {this.determineSelectedProducts().length} results</span>
+              <select 
+                onChange={(e) => this.updateSort(e.target.value)}
+                className="sort-dropdown"
+              >
+                <option value="name_a_z">Sort By: Name (A-Z)</option>
+                <option value="name_z_a">Name (Z-A)</option>
+                <option value="price_l_h">Price (Low to High)</option>
+                <option value="price_h_l">Price (High to Low)</option>
+              </select>
+            </div>
 
-        return updatedSelectedProducts
-    }
-
-
-    updateSearchQuery = e => {
-        this.setState({searchQuery: e.target.value})
-        this.determineSelectedProducts()
-    }
-
-    updateSort = val => {
-        switch (val) {
-            case "name_a_z":
-                this.setState({sortField: "product_name", sortDirection: 1})
-                break
-            case "name_z_a":
-                this.setState({sortField: "product_name", sortDirection: -1})
-                break
-            case "price_l_h":
-                this.setState({sortField: "product_price", sortDirection: 1})
-                break
-            case "price_h_l":
-                this.setState({sortField: "product_price", sortDirection: -1})
-                break
-
-        }
-    }
-
-    toggleTag = tagName => {
-        if (!this.state.selectedTags.includes(tagName)) {
-            this.setState({selectedTags: [...this.state.selectedTags, tagName]})
-        } else {
-            let i = this.state.selectedTags.indexOf(tagName)
-            let newTagsList = [...this.state.selectedTags]
-            newTagsList.splice(i, 1)
-            this.setState({selectedTags: newTagsList})
-        }
-        console.log(this.state.selectedTags)
-    }
-
-    sortProducts = productsList => {
-        return productsList.sort((a, b) =>
-            a[this.state.sortField] > b[this.state.sortField] ? this.state.sortDirection : -this.state.sortDirection)
-    }
-
-    componentDidMount() {
-        axios.get(`${SERVER_HOST}/products`)
-            .then(res => {
-                if (res.data) {
-                    if (res.data.errorMessage) {
-                        console.log(res.data.errorMessage)
-                    } else {
-                        this.setState({products: res.data})
-                    }
-                }
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
-
-
-    render() {
-        return (
-            <>
-                <Header/>
-                <SearchTools searchQuery={this.state.searchQuery}
-                             updateSearchQuery={this.updateSearchQuery}
-                             toggleTag={this.toggleTag}
-                             updateSort={this.updateSort}
-                />
-                <div id="productsDisplayPanel">
-                    {this.sortProducts(this.determineSelectedProducts()).map(product => <ProductDisplayCard
-                        key={product._id} product={product}/>)}
-                </div>
-                <PageFooter/>
-            </>
-        )
-    }
+            {/* ----------product cards*/}
+            <div className="products-grid">
+              {this.sortProducts(this.determineSelectedProducts()).map(product => (
+                <ProductDisplayCard key={product._id} product={product} />
+              ))}
+            </div>
+          </div>
+        </div>
+        <PageFooter />
+      </div>
+    );
+}
 }
--- client/src/components/SearchTools.js
+++ client/src/components/SearchTools.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import TagCheckBox from "./TagCheckBox"
 
 export default class SearchTools extends Component {
-
     constructor(props) {
         super(props)
         this.state = {
@@ -18,42 +17,40 @@ export default class SearchTools extends Component {
 
     render() {
         return (
-            <div className="container">
-                <div id="searchToolsContainer" className="search-tools-container">
-                    <div className="search-row">
-                        <input
-                            type="text"
-                            value={this.props.searchQuery}
-                            onChange={(e) => this.props.updateSearchQuery(e)}
-                            placeholder="Search"
-                            className="search-input"
-                        />
+            <div className="search-tools-wrapper">
+                <div className="search-sort-row">
+                    <input
+                        type="text"
+                        value={this.props.searchQuery}
+                        onChange={(e) => this.props.updateSearchQuery(e)}
+                        placeholder="Search products..."
+                        className="filter-search-input"
+                    />
+                    
+                    {/* <select
+                        value={this.state.filter}
+                        onChange={this.updateSortField}
+                        className="filter-sort-dropdown"
+                    >
+                        <option value="default">Sort By</option>
+                        <option value="name_a_z">Name: A-Z</option>
+                        <option value="name_z_a">Name: Z-A</option>
+                        <option value="price_l_h">Price: Low - High</option>
+                        <option value="price_h_l">Price: High - Low</option>
+                    </select> */}
+                </div>
 
-                        <select
-                            value={this.state.filter}
-                            onChange={this.updateSortField}
-                            className="sort-dropdown"
-                        >
-                            <option value="default">Sort By</option>
-                            <option value="name_a_z">Name: A-Z</option>
-                            <option value="name_z_a">Name: Z-A</option>
-                            <option value="price_l_h">Price: Low - High</option>
-                            <option value="price_h_l">Price: High - Low</option>
-                        </select>
-                    </div>
-
-                    <div className="filter-container">
-                        <h4 className="filter-title">Filter Results</h4>
-                        <div className="filter-tags">
-                            {this.tags.map((tag) => (
-                                <TagCheckBox
-                                    key={`${tag}_cb`}
-                                    tagname={tag}
-                                    name={tag}
-                                    toggleTag={this.props.toggleTag}
-                                />
-                            ))}
-                        </div>
+                <div className="filter-section">
+                    <h3 className="filter-header">Filter by Category</h3>
+                    <div className="filter-tags-grid">
+                        {this.tags.map((tag) => (
+                            <TagCheckBox
+                                key={`${tag}_cb`}
+                                tagname={tag}
+                                name={tag}
+                                toggleTag={this.props.toggleTag}
+                            />
+                        ))}
                     </div>
                 </div>
             </div>
