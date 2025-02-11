41285a07b1cb127816e84261d43ccc9a81c717c2
Jose Henrique Joanoni <henrique.joanoni@gmail.com>
2025-02-11 21:43:13 +0000

    refactor(server): Comment remote mongo connection
    Remote mongodb connection commented in order to merge to develop and don't break others project

--- server/config/dbconn.js
+++ server/config/dbconn.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose')
 
 /** connection URI needs to be set this way to work on localhost */
-// mongoose.connect(`mongodb://0.0.0.0:27017/${process.env.DB_NAME}`, {
-//     useNewUrlParser: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-// })
-
-/** connection URI remote cluster */
-mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster-firestrings.micxw.mongodb.net/${process.env.DB_NAME}`, {
+mongoose.connect(`mongodb://0.0.0.0:27017/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
 })
+
+/** connection URI remote cluster */
+// mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@cluster-firestrings.micxw.mongodb.net/${process.env.DB_NAME}`, {
+//     useNewUrlParser: true,
+//     useFindAndModify: false,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+// })
 
 const db = mongoose.connection
 db.on('error', console.log.bind(console, 'connection error:'))
