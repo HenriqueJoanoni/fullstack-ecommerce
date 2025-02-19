7ea156eb1698ad8b55696adf1c2ff6cb5056d3a9
Jose Henrique Joanoni <henrique.joanoni@gmail.com>
2025-02-19 00:50:53 +0000

    feat(client): Favorites Page - needs fixing

--- client/src/components/FavoritesPage.jsx
+++ client/src/components/FavoritesPage.jsx
@@ -1,20 +1,56 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Header from "./Header";
 import PageFooter from "./PageFooter";
 
 export default class FavoritesPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            favData: null,
+        };
+    }
+
+    componentDidMount() {
+        console.log("props location: ", this.props.location)
+        const { state } = this.props.location;
+
+        if (state && state.favorites) {
+            console.log("Favorites Data: ", state.favorites);
+            this.setState({ favData: state.favorites });
+        }
     }
 
     render() {
+        const { favData } = this.state;
+
+        // const isFavoritesValid = favData && typeof favData === "object" && Object.keys(favData).length > 0;
+
         return (
             <>
-                <Header/>
-                <h1>THIS IS THE FAV PAGE</h1>
-                <PageFooter/>
+                <div className="app-container">
+                    <div className="header-container">
+                        <Header />
+                    </div>
+                    <section className="favorites-section">
+                        <h1 className="favorites-title">Favorites</h1>
+
+                        <div className="container">
+                            {/*{isFavoritesValid ? (*/}
+                            {/*    <div>*/}
+                            {/*        <h2>{favData.product_name}</h2>*/}
+                            {/*        <p>{favData.product_description}</p>*/}
+                            {/*        <p>Price: ${favData.product_price}</p>*/}
+                            {/*        <img src={favData.product_picture[0]} alt={favData.product_name} />*/}
+                            {/*        /!* Add more details as needed *!/*/}
+                            {/*    </div>*/}
+                            {/*) : (*/}
+                            {/*    <p>No Favorites yet :(</p>*/}
+                            {/*)}*/}
+                        </div>
+                    </section>
+                </div>
+                <PageFooter />
             </>
-        )
+        );
     }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -173,4 +173,23 @@ router.post("/profile-update", verifyTokenPassword, async (req, res) => {
     }
 })
 
+/** DISPLAY FAVORITES */
+router.get("/favorites/:email", verifyTokenPassword, async (req, res) => {
+    try {
+        const userFavs = await User.findOne({user_email: req.params.email}).populate("favorites")
+
+        if (!userFavs) {
+            return res.status(404).json({error: "User not found"})
+        }
+
+        res.status(200).json({
+            favorites: userFavs.favorites
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: "Internal server error"})
+    }
+})
+
 module.exports = router
